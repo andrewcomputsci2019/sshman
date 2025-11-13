f10762edc1de0154935895d8afdbebb19f431fbb
Andrew <54915639+andrewcomputsci2019@users.noreply.github.com>
2025-11-12 19:24:58 -0800

    updated config file, removed pointless conflict level. Also removed sync to ssh config file, instead user should use include directive.

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,7 +4,6 @@ type ConflictPolicy string
 
 const (
 	ConflictIgnore      ConflictPolicy = "ignore"       // ignore conflicts dont sync data stores
-	ConflictFavorDB     ConflictPolicy = "favor_db"     // conflicts are resolved with using db version
 	ConflictFavorConfig ConflictPolicy = "favor_config" // conflicts are resolved with using config version
 	ConflictAlwaysError ConflictPolicy = "always_error" // if there's conflict error the program
 )
@@ -13,18 +12,18 @@ type Config struct {
 	SshConf     SshConfig     `yaml:"ssh_config"`
 	StorageConf StorageConfig `yaml:"storage_config"`
 	Ssh         SSH           `yaml:"ssh"`
+	EnablePing  bool          `yaml:"enable_ping"` // trys to ping host to see if they are connected and reports their ping
 }
 
 type SshConfig struct {
 	SshConfigEnabled bool   `yaml:"enabled"`
 	SshConfigPath    string `yaml:"config_path,omitempty"`
-	Sync             bool   `yaml:"sync,omitempty"` // syncs backing storage to this config
 }
 
 type StorageConfig struct {
 	StoragePath    string `yaml:"storage_path,omitempty"`
-	WriteThrough   bool   `yaml:"write_through,omitempty"`
-	ConflictPolicy string `yaml:"conflict_policy,omitempty"`
+	WriteThrough   *bool  `yaml:"write_through,omitempty"`   // by default WriteThrough is considered True
+	ConflictPolicy string `yaml:"conflict_policy,omitempty"` // if not provided or illegal type defaults to ignore
 }
 
 type SSH struct {
