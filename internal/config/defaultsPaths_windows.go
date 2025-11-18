7a8e0ec68201304413a631c563f4bf2938040d3a
Andrew <54915639+andrewcomputsci2019@users.noreply.github.com>
2025-11-17 23:32:09 -0800

    no longer need config path

--- internal/config/defaultsPaths_windows.go
+++ internal/config/defaultsPaths_windows.go
@@ -1,8 +1,6 @@
 package config
 
 const (
-	// DefaultConfigPath Should be prefixed with USERPROFILE
-	DefaultConfigPath = "\\.ssh\\config"
 	// DefaultAppConfigPath Should be prefixed with AppData
 	DefaultAppConfigPath = "ssh_man\\"
 	// DefaultAppStorePath points to the path where app storage should go, note this should be prefixed with %APPDATA%
