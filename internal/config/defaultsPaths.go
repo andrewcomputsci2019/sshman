60a447074aa6d05547d0187d1cb08ca104328755
Andrew <54915639+andrewcomputsci2019@users.noreply.github.com>
2025-11-17 23:32:04 -0800

    no longer need config path

--- internal/config/defaultsPaths.go
+++ internal/config/defaultsPaths.go
@@ -3,8 +3,6 @@
 package config
 
 const (
-	// DefaultConfigPath should be prefixed with $HOME
-	DefaultConfigPath = "/.ssh/config"
 	// DefaultAppConfigPath should be prefixed with XDG_CONFIG_HOME
 	DefaultAppConfigPath = "ssh_man/config.yml"
 	// DefaultAppStorePath should be prefixed with XDG_DATA_HOME
