8e3fb3924b711a167dbfb709eb0167fda586367a
Andrew <54915639+andrewcomputsci2019@users.noreply.github.com>
2025-11-25 11:22:57 -0800

    finished option's validator. Added support for validating ssh forwarding rules and ports, supports v6 and v4 ips and supports dynamic forwarding socks rules

--- internal/sshUtils/optionValidator.go
+++ internal/sshUtils/optionValidator.go
@@ -8,6 +8,7 @@ import (
 	"slices"
 	"strconv"
 	"strings"
+	"unicode"
 )
 
 var validOptionsToBeSet = map[string]struct{}{
@@ -31,8 +32,6 @@ var validOptionsToBeSet = map[string]struct{}{
 	"DynamicForward":               {},
 	"ForwardX11":                   {},
 	"ForwardX11Timeout":            {},
-	"GlobalKnownHostsFile":         {},
-	"HostKeyAlgorithms":            {},
 	"HostKeyAlias":                 {},
 	"KbdInteractiveAuthentication": {},
 	"LocalForward":                 {},
@@ -69,8 +68,6 @@ func GetListOfAcceptableOptions() []string {
 
 	return slices.Sorted(maps.Keys(validOptionsToBeSet))
 }
-
-// todo add validators for common options
 
 func isAddressFamilyValid(family string) bool {
 	return family == "any" || family == "inet" || family == "inet6"
@@ -154,8 +151,6 @@ func isCompressionModeValid(mode string) bool {
 	return yesNoOptionValid(mode)
 }
 
-// todo finish other validation methods
-
 func isChannelTimeoutValid(channelTimeout string) bool {
 	//timeouts as quoted from manpage is seperated by whitespace and in the form of type=interval
 	pairs := strings.Split(channelTimeout, " ")
@@ -191,12 +186,6 @@ func isConnectTimeoutValid(connectTimeout string) bool {
 	return true
 }
 
-func isDynamicForwardValid(dynamicForward string) bool {
-
-	// todo validate this input
-	panic("implement me")
-}
-
 func isForwardX11Valid(forwardX11 string) bool {
 	return yesNoOptionValid(forwardX11)
 }
@@ -205,4 +194,144 @@ func isForwardX11TimeoutValid(forwardX11Timeout string) bool {
 	return validSSHTime(forwardX11Timeout)
 }
 
-// todo continue more validation after this point
+func isHostKeyAliasValid(hostKeyAlias string) bool {
+	return len(hostKeyAlias) > 0
+}
+
+func isKbdInteractiveAuthenticationValid(kbdInteractiveAuthentication string) bool {
+	return yesNoOptionValid(kbdInteractiveAuthentication)
+}
+
+func isPasswordAuthenticationValid(passwordAuthentication string) bool {
+	return yesNoOptionValid(passwordAuthentication)
+}
+
+// splitForwardSpec splits a forwarding spec supports ipv6 host blocks.
+// Example: "[::1]:8080:[2001:db8::1]:22"
+func splitForwardSpec(s string) []string {
+	var parts []string
+	var strBuilder strings.Builder
+	inBrackets := false
+
+	for _, r := range s {
+		switch r {
+		case '[':
+			inBrackets = true
+			strBuilder.WriteRune(r)
+		case ']':
+			inBrackets = false
+			strBuilder.WriteRune(r)
+		case ':':
+			if inBrackets {
+				strBuilder.WriteRune(r)
+			} else {
+				parts = append(parts, strBuilder.String())
+				strBuilder.Reset()
+			}
+		default:
+			strBuilder.WriteRune(r)
+		}
+	}
+	parts = append(parts, strBuilder.String())
+
+	return parts
+}
+
+func isValidPort(port string) bool {
+	n, err := strconv.Atoi(port)
+	if err != nil {
+		return false
+	}
+	return n > 0 && n <= 65535
+}
+
+func isValidHostIP(h string) bool {
+	if strings.HasPrefix(h, "[") && strings.HasSuffix(h, "]") {
+		ip, err := netip.ParseAddr(h[1 : len(h)-1])
+		if err != nil {
+			return false
+		}
+		return ip.Is6()
+	}
+
+	// check if ipv4
+	// needs to be in form of quads so 255.255.255.255 can drop leading zeros
+	ip, err := netip.ParseAddr(h)
+	if err == nil && ip.Is4() {
+		return true
+	}
+	return false
+}
+
+func isValidHostname(h string) bool {
+	// total host length has to be less than 254 chars
+	if len(h) == 0 || len(h) > 253 {
+		return false
+	}
+	parts := strings.Split(h, ".") // get all parts of the hostname
+	for _, part := range parts {
+		if len(part) == 0 {
+			return false
+		}
+		if len(part) > 63 {
+			return false
+		}
+
+		if !(unicode.IsDigit(rune(part[0])) || unicode.IsLetter(rune(part[0]))) ||
+			!(unicode.IsDigit(rune(part[len(part)-1])) || unicode.IsLetter(rune(part[len(part)-1]))) {
+			return false
+		}
+		for _, char := range part {
+			if unicode.IsLetter(char) || unicode.IsDigit(char) {
+				continue
+			}
+			if char == '-' {
+				continue
+			}
+			return false
+		}
+	}
+	return true
+}
+
+func validHost(h string) bool {
+	return isValidHostIP(h) || isValidHostname(h)
+}
+
+func isLocalForwardValid(localForward string) bool {
+	// so we should either get [bindAddr]:port:[bindAddr]:port
+	// or port:bindAddr:port
+	parts := splitForwardSpec(localForward)
+	if len(parts) != 3 && len(parts) != 4 {
+		return false
+	}
+	if len(parts) == 3 { //short for ie port:host:port
+		return isValidPort(parts[0]) && validHost(parts[1]) && isValidPort(parts[2])
+	} else {
+		return validHost(parts[0]) && isValidPort(parts[1]) && validHost(parts[2]) && isValidPort(parts[3])
+	}
+}
+
+func isRemoteForwardValid(remoteForward string) bool {
+	parts := splitForwardSpec(remoteForward)
+	if len(parts) != 3 && len(parts) != 4 {
+		return false
+	}
+	if len(parts) == 3 { //short for ie port:host:port
+		return isValidPort(parts[0]) && validHost(parts[1]) && isValidPort(parts[2])
+	} else {
+		return validHost(parts[0]) && isValidPort(parts[1]) && validHost(parts[2]) && isValidPort(parts[3])
+	}
+}
+
+func isDynamicForwardValid(dynamicForward string) bool {
+	parts := splitForwardSpec(dynamicForward)
+	if len(parts) > 1 {
+		return false
+	}
+	if len(parts) == 1 {
+		return validHost(parts[0]) && isValidPort(parts[1])
+	} else {
+		return isValidPort(parts[0])
+	}
+}
