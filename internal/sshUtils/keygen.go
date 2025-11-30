4e3acbb323c97bf0ba15f5701f1e3da40a239b5e
Andrew <54915639+andrewcomputsci2019@users.noreply.github.com>
2025-11-30 10:49:45 -0800

    added main tui file, and added keygen file for future filling in

---
+++ internal/sshUtils/keygen.go
@@ -0,0 +1,5 @@
+package sshUtils
+
+// todo add code here to automatically gen keys etc
+
+// basically wrap ssh-keygen
---
+++ internal/tui/tui.go
@@ -0,0 +1,14 @@
+package tui
+
+import (
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type AppModel struct {
+}
+
+// this is where bubble tea related code will go
+// todo write need tui code and remove func below
+func thing() {
+	tea.ClearScreen()
+}
