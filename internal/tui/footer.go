7048e3b18000053a8390b3d02b1e3bb11cb80bd4
Andrew Pegg <andrewcomputsci2019@gmail.com>
2025-12-10 00:31:25 -0800

    finished footer

--- internal/tui/footer.go
+++ internal/tui/footer.go
@@ -6,25 +6,39 @@ import (
 )
 
 type FooterModel struct {
-	height int
-	help.Model
+	height, width int
+	h             help.Model
+	currentKeymap help.KeyMap
+}
+
+func NewFooterModel() FooterModel {
+	return FooterModel{
+		height: 1,
+	}
 }
 
 func (f FooterModel) Init() tea.Cmd {
-	//TODO implement me
-	panic("implement me")
+	return nil
 }
 
 func (f FooterModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	//TODO implement me
-	panic("implement me")
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		// the width should already be adjusted for the appended on borders
+		f.width = msg.Width
+		f.h.Width = msg.Width
+	}
+	return f, nil
 }
 
 func (f FooterModel) View() string {
-	//TODO implement me
-	panic("implement me")
+	return f.h.View(f.currentKeymap)
 }
 
-func (h FooterModel) Height() int {
-	return h.height
+func (f FooterModel) Height() int {
+	return f.height
+}
+
+func (f FooterModel) Width() int {
+	return f.width
 }
