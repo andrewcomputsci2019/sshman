11381921a4e2a2dcbdf4619e73a6412895a3cbd8
Andrew Pegg <andrewcomputsci2019@gmail.com>
2025-12-20 23:40:16 -0800

    test for hosts dao completed, passing all

--- internal/sqlite/hostDao_test.go
+++ internal/sqlite/hostDao_test.go
@@ -2,6 +2,7 @@ package sqlite
 
 import (
 	"log"
+	"slices"
 	"strconv"
 	"testing"
 	"time"
@@ -91,12 +92,155 @@ func TestUpdateHost(t *testing.T) {
 
 func TestDeleteHost(t *testing.T) {
 
+	db := NewHostDao(conn)
+	err := db.Delete(Host{Host: "Test"})
+	if err != nil {
+		t.Fatalf("Failed to delete Host: Test from db")
+	}
+	_, err = db.Get("Test")
+	if err == nil {
+		t.Fatalf("Host should not be fetchable from db")
+	}
+
+}
+
+func TestInsertMany(t *testing.T) {
+	host1 := Host{
+		Host:      "Host_TEST1",
+		Tags:      []string{"TEST1"},
+		CreatedAt: time.Now(),
+		Notes:     "HOST 1 test",
+		Options:   []HostOptions{{Key: "User", Value: "MyUser"}},
+	}
+	host2 := Host{
+		Host:      "Host_TEST2",
+		Tags:      []string{"TEST2"},
+		CreatedAt: time.Now(),
+		Notes:     "HOST 2 Test",
+		Options:   []HostOptions{{Key: "User", Value: "MyUser"}},
+	}
+	db := NewHostDao(conn)
+	err := db.InsertMany(host1, host2)
+	if err != nil {
+		t.Fatalf("Failed to insert multiple host into database. Error: %v", err)
+	}
+
+}
+
+func TestUpdateMany(t *testing.T) {
+
+}
+
+func TestUpsertHost(t *testing.T) {
+
+	host1 := Host{
+		Host:      "Host_TEST1",
+		Tags:      []string{"TEST1", "UPSERT"},
+		CreatedAt: time.Now(),
+		Notes:     "HOST 1 upsert test",
+		Options:   []HostOptions{{Key: "User", Value: "UpsertHost"}},
+	}
+
+	db := NewHostDao(conn)
+	err := db.InsertOrUpdate(host1)
+	if err != nil {
+		t.Fatalf("Failed to upsert host into table. err: %v", err)
+	}
+	updatedHost, err := db.Get("Host_TEST1")
+	if err != nil {
+		t.Fatalf("failed to grab updated host from db")
+	}
+	if updatedHost.Tags[1] != "UPSERT" || updatedHost.Options[0].Value != "UpsertHost" || updatedHost.Notes != "HOST 1 upsert test" {
+		t.Fatalf("Host was not properly updated. Host: %v", updatedHost)
+	}
+	log.Printf("Updated host %v", updatedHost)
+}
+
+func TestInsertIgnoreConflict(t *testing.T) {
+	host1 := Host{
+		Host:      "Host_TEST1",
+		Tags:      []string{"TEST1"},
+		CreatedAt: time.Now(),
+		Notes:     "Should not be inserted into db",
+		Options:   []HostOptions{{Key: "User", Value: "MyUser"}},
+	}
+	newHost := Host{
+		Host:      "New_Host",
+		Tags:      []string{"Test"},
+		CreatedAt: time.Now(),
+		Notes:     "Should be inserted",
+	}
+	db := NewHostDao(conn)
+	err := db.InsertManyIgnoreConflict(host1, newHost)
+	if err != nil {
+		t.Fatalf("Failed to insert many host with conflict. Error %v", err)
+	}
+	validate, err := db.Get("Host_TEST1")
+	if err != nil {
+		t.Fatalf("Failed to get Host from db")
+	}
+	if validate.Notes == "Should not be inserted into db" {
+		t.Fatalf("Test failed, host should have not been inserted into the table")
+	}
+}
+
+func TestUpsertMany(t *testing.T) {
+	db := NewHostDao(conn)
+	newHost, err := db.Get("New_Host")
+	if err != nil {
+		t.Fatal(err)
+	}
+	host1, err := db.Get("Host_TEST1")
+	if err != nil {
+		t.Fatal(err)
+	}
+	timeStamp := time.Now().Add(time.Second * 2) // give a buffer so clock doesn't return the same time twice
+	host1.UpdatedAt = &timeStamp
+	newHost.UpdatedAt = &timeStamp
+
+	host1.Tags = append(host1.Tags, "Many")
+	newHost.Tags = append(newHost.Tags, "Many")
+
+	err = db.InsertOrUpdateMany(newHost, host1)
+	if err != nil {
+		t.Fatalf("Failed to upsert multiple host into db")
+	}
+	valid1, _ := db.Get("New_Host")
+	valid2, _ := db.Get("Host_TEST1")
+
+	if slices.Compare(valid1.Tags, newHost.Tags) != 0 || slices.Compare(valid2.Tags, host1.Tags) != 0 {
+		t.Fatalf("Host should be updated yet they are not")
+	}
+	log.Printf("Valid 1 %v", valid1)
+	log.Printf("Valid 2 %v", valid2)
+	if !valid1.UpdatedAt.After(valid1.CreatedAt) || !valid2.UpdatedAt.After(valid2.CreatedAt) {
+		t.Fatalf("Updated at should be newer than created timestamp")
+	}
+
 }
 
 func TestGetAll(t *testing.T) {
-
+	hosts, err := NewHostDao(conn).GetAll()
+	if err != nil {
+		t.Fatalf("Failed to get all hosts. error %v", err)
+	}
+	t.Logf("Hosts returned by database: %v", hosts)
+	if len(hosts) == 0 {
+		t.Fatalf("Should be a filled slice")
+	}
 }
 
 func TestCountAll(t *testing.T) {
+	hosts, err := NewHostDao(conn).GetAll()
+	if err != nil {
+		t.Fatalf("Failed to get all hosts. error %v", err)
+	}
+	count, err := NewHostDao(conn).Count()
+	if err != nil {
+		t.Fatalf("Failed to get count of database. Error: %v", err)
+	}
+	if len(hosts) != int(count) {
+		t.Fatalf("db mismatch between count and return hosts. Expected %v, got %v hosts", count, len(hosts))
+	}
 
 }
