d0e04dbbfc76bd48cb168ac6a9e5e55d2c4ba26f
Andrew Pegg <andrewcomputsci2019@gmail.com>
2025-12-21 23:44:20 -0800

    added unit test for ssh config parser

--- internal/sshParser/configParser_test.go
+++ internal/sshParser/configParser_test.go
@@ -1,9 +1,13 @@
 package sshParser
 
 import (
+	"andrew/sshman/internal/sqlite"
 	"os"
 	"path/filepath"
 	"testing"
+	"time"
+
+	"github.com/kevinburke/ssh_config"
 )
 
 func TestReadConfig_SingleHost(t *testing.T) {
@@ -80,5 +84,127 @@ Host staging.example.com
 
 	if len(hosts[1].Options) != 3 {
 		t.Fatalf("expected 3 hosts options but got %v", len(hosts[1].Options))
+	}
+}
+
+func TestSerializeHostToSshHost(t *testing.T) {
+	host := sqlite.Host{
+		Host:      "test.local",
+		CreatedAt: time.Now(),
+		Options: []sqlite.HostOptions{{
+			Key:   "User",
+			Value: "MyUser",
+		}},
+		Notes: "These should be at the bottom of the host",
+	}
+	hostStr, err := serializeHostToSshHost(&host)
+	if err != nil {
+		t.Fatalf("Failed to serialize ssh host object to ssh parsed form")
+	}
+	for _, node := range hostStr.Nodes {
+		if comment, ok := node.(*ssh_config.Empty); ok {
+			if comment.Comment != "These should be at the bottom of the host" {
+				t.Fatalf("Comment should only be the string %s", "These should be at the bottom of the host")
+			}
+		}
+	}
+	t.Logf("converted host to string: %v", hostStr)
+}
+
+// todo add test for adding files into config file
+func TestAddHostToFile(t *testing.T) {
+	dir := t.TempDir()
+	file := filepath.Join(dir, "new_config")
+	host := sqlite.Host{
+		Host:      "test.local",
+		CreatedAt: time.Now(),
+		Options: []sqlite.HostOptions{{
+			Key:   "User",
+			Value: "MyUser",
+		}},
+		Notes: "These should be at the bottom of the host",
+	}
+	err := AddHostToFile(file, host)
+	if err != nil {
+		t.Fatalf("failed to add host into config file. Error %v", err)
+	}
+	// now we need to check if host exist in file
+	hosts, err := ReadConfig(file)
+	if err != nil {
+		t.Fatalf("Failed to parse file after writing host to file. Error: %v", err)
+	}
+	if len(hosts) != 1 {
+		t.Fatalf("Should only be 1 host inside of the file")
+	}
+	if hosts[0].Host != "test.local" {
+		t.Fatalf("Hostname is not correct: Expected %s but got %s", "test.local", hosts[0].Host)
+	}
+	if len(hosts[0].Options) != 1 {
+		t.Fatalf("Hosts options should be of length 1 but was %v", len(hosts[0].Options))
+	}
+	if hosts[0].Options[0].Key != "User" || hosts[0].Options[0].Value != "MyUser" {
+		t.Fatalf("Host option was not parsed correctly. %v,%v", hosts[0].Options[0].Key, hosts[0].Options[0].Value)
+	}
+	if hosts[0].Notes != "These should be at the bottom of the host" {
+		t.Fatalf("Host Notes are not correct")
+	}
+}
+
+func TestSerializeHostToFile(t *testing.T) {
+	dir := t.TempDir()
+	file := filepath.Join(dir, "dump_config")
+	host := sqlite.Host{
+		Host:      "test.local",
+		CreatedAt: time.Now(),
+		Options: []sqlite.HostOptions{{
+			Key:   "User",
+			Value: "MyUser",
+		}},
+		Notes: "These should be at the bottom of the host",
+	}
+	host2 := sqlite.Host{
+		Host:      "test2.local",
+		CreatedAt: time.Now(),
+		Options: []sqlite.HostOptions{{
+			Key:   "User",
+			Value: "MyUser2",
+		}},
+		Notes: "These should be at the bottom of host2",
+	}
+	err := SerializeHostToFile(file, []sqlite.Host{host, host2})
+	if err != nil {
+		t.Fatalf("Failed to write out ssh host objects to config file. Error %v", err)
+	}
+	hosts, err := ReadConfig(file)
+	if err != nil {
+		t.Fatalf("Failed to parse file after writing host to file. Error: %v", err)
+	}
+	if len(hosts) != 2 {
+		t.Fatalf("Should only be 2 host inside of the file")
+	}
+	if hosts[0].Host != "test.local" {
+		t.Fatalf("Hostname is not correct: Expected %s but got %s", "test.local", hosts[0].Host)
+	}
+	if len(hosts[0].Options) != 1 {
+		t.Fatalf("Hosts options should be of length 1 but was %v", len(hosts[0].Options))
+	}
+	if hosts[0].Options[0].Key != "User" || hosts[0].Options[0].Value != "MyUser" {
+		t.Fatalf("Host option was not parsed correctly. %v,%v", hosts[0].Options[0].Key, hosts[0].Options[0].Value)
+	}
+	if hosts[0].Notes != "These should be at the bottom of the host" {
+		t.Fatalf("Host Notes are not correct. Notes %v", hosts[0].Notes)
+	}
+
+	if hosts[1].Host != "test2.local" {
+		t.Fatalf("Hostname is not correct: Expected %s but got %s", "test.local", hosts[0].Host)
+	}
+	if len(hosts[1].Options) != 1 {
+		t.Fatalf("Hosts options should be of length 1 but was %v", len(hosts[1].Options))
+	}
+	if hosts[1].Options[0].Key != "User" || hosts[1].Options[0].Value != "MyUser2" {
+		t.Fatalf("Host option was not parsed correctly. %v,%v", hosts[1].Options[0].Key, hosts[1].Options[0].Value)
+	}
+	if hosts[1].Notes != "These should be at the bottom of host2" {
+		t.Fatalf("Host Notes are not correct. Notes %v", hosts[1].Notes)
 	}
 }
