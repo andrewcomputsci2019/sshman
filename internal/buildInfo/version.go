ba263038e18b311097ed4c802f7f5c779659c454
Andrew <54915639+andrewcomputsci2019@users.noreply.github.com>
2026-03-07 16:21:27 -0800

    bump to v1.1.1

--- internal/buildInfo/version.go
+++ internal/buildInfo/version.go
@@ -9,7 +9,7 @@ const BuildMajor = 1
 const BuildMinor = 1
 
 // BuildPatch patch build version
-const BuildPatch = 0
+const BuildPatch = 1
 
 // BuildDate specifies the build date of the application
 var BuildDate string
